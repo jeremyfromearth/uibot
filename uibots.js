f9734756f08b22aecb99a8f812de15202c669290
jeremyfromearth <jeremynealbrown@gmail.com>
2016-08-24 09:29:01 -0700

    Added string and select component and two-way data bindings

--- uibots.js
+++ uibots.js
@@ -8,7 +8,6 @@ function UIBot() {
         if(!target.hasOwnProperty([param.name])) {
             console.log('UIBots Warning: target does not contain property: ', param.name);
         } else {
-            console.log('Creating UI Element For Param:', param);
             input = null;
             type = typeof(target[param.name]); 
             switch(type) {
@@ -51,7 +50,6 @@ function UIBot() {
 
         input.addEventListener('change', function(event) {
             target[param.name] = input.checked;
-            console.log(input.checked);
         });
 
         bindings.push(function() {
@@ -69,7 +67,7 @@ function UIBot() {
         input.type = 'button';
         input.param = param;
         input.value = param.label;
-        input.className = 'uibot-button';
+        input.className = 'uibot_button';
         
         div.appendChild(input);
         container.appendChild(div);
@@ -83,7 +81,7 @@ function UIBot() {
     }
 
     function createNumberComponent(target, param, container) {
-        if(param.options) {
+        if(param.hasOwnProperty('options')) {
 
         } else {
             param.step = param.step || 0.1;
@@ -143,15 +141,109 @@ function UIBot() {
     }
 
     function createStringComponent(target, param, container) {
+        if(param.hasOwnProperty('options')) {
+            createSelectComponent(target, param, container);
+        } else {
+            var div = document.createElement('div');
+            div.className = 'uibot_component_container';
 
+            var label = document.createElement('div');
+            label.className = 'uibot_component_label';
+            label.innerHTML = param.label;
+
+            var input = document.createElement('input');
+            input.className = 'uibot_text_input';
+            input.type = 'text';
+            input.value = target[param.name];
+
+            if(param.hasOwnProperty('max_length')) {
+                input.maxLength = param.max_length;
+            }
+
+            div.appendChild(label);
+            div.appendChild(input);
+            container.appendChild(div);
+            
+            input.addEventListener('change', function(event) {
+                target[param.name] = input.value;
+            });
+
+            var focus = false;
+            input.addEventListener('focus', function(event) {
+                focus = true;
+            });
+
+            input.addEventListener('blur', function(event) {
+                focus = false;
+            });
+
+            bindings.push(function() {
+                if(!focus) input.value = target[param.name];
+            });
+
+            return input;
+        }
     }
 
     function createSelectComponent(target, param, container) {
+        var div = document.createElement('div');
+        div.className = 'uibot_component_container';
 
+        var label = document.createElement('div');
+        label.className = 'uibot_component_label';
+        label.innerHTML = param.label;
+
+        var select = document.createElement('select');
+        select.className = 'uibot_select';
+
+        for(var i = 0; i < param.options.length; i++) {
+            var option = document.createElement('option');
+            option.value = param.options[i];
+            option.innerHTML = param.options[i];
+            select.appendChild(option);
+        }
+        select.selectedIndex = param.options.indexOf(target[param.name]);
+
+        div.appendChild(label);
+        div.appendChild(select);
+        container.appendChild(div);
+
+        select.addEventListener('change', function(event) {
+            target[param.name] = param.options[select.selectedIndex];
+        })
+
+        var focus = false;
+        select.addEventListener('focus', function(event) {
+            focus = true;
+        });
+
+        select.addEventListener('blur', function(event) {
+            focus = false;
+        });
+
+        bindings.push(function() {
+            if(!focus) {
+                var index = param.options.indexOf(target[param.name]);
+                select.selectedIndex = index;
+            }
+        });
+    }
+
+    function bind() {
+        bindId = setInterval(function() {
+            for(var i = 0; i < bindings.length; i++) {
+                bindings[i]();
+            }
+        }, 500);
+    }
+
+    function unbind() {
+        clearInterval(bindId);
     }
 
     return {
         id : uibotId,
+        bind : bind,
         build : function(target, params, container) {
             for(var name in params) {
                 var param = params[name];
@@ -165,6 +257,7 @@ function UIBot() {
                     createUIElement(target, param, container);
                 }
             }
-        }
+        },
+        unind : unbind
     }
 }
